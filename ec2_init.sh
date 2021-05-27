e14c3ea1d6a319923dbf13517fa0fbc7e9d8b3c5
rdz <rdszhao@gmail.com>
2021-05-26 21:38:48 -0700

    temp

--- ec2_init.sh
+++ ec2_init.sh
@@ -2,7 +2,9 @@
 python3 -m venv /home/ec2-user/dsc
 source /home/ec2-user/dsc/bin/activate
 pip install 'dask[complete]'
+pip install dask_ml
 pip install sklearn
 pip install s3fs
+pip install pyarrow
 pip install awscli --upgrade
 git clone https://github.com/rdszhao/DSC102_Project.git /home/ec2-user/DSC102_Project
