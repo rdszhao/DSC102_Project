2f0fa098a91159f6b7362f5453c1cc647a9c8a02
rdz <rdszhao@gmail.com>
2021-05-28 21:09:20 -0700

    rm rm

--- client.sh
+++ client.sh
@@ -1,6 +1,6 @@
 aws ec2 run-instances \
     --launch-template LaunchTemplateId=lt-0931323da9297894e
-sleep 65s
+sleep 100s
 instanceid=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId" --output text)
 address=$(aws ec2 describe-instances --instance-ids $instanceid --query "Reservations[*].Instances[*].PublicDnsName" --output text)
 ssh -o StrictHostKeyChecking=no -i '~/aws-keys/ray-key.pem' ec2-user@$address 'git clone https://github.com/rdszhao/DSC102_Project.git /home/ec2-user/DSC102_Project'
--- modules/feature_prep.py
+++ modules/feature_prep.py
@@ -1,4 +1,3 @@
-# %%
 from dask.diagnostics import ProgressBar
 import dask.dataframe as dd
 # from dask.distributed import LocalCluster, Client
@@ -7,7 +6,7 @@ from dask_ml.preprocessing import Categorizer, DummyEncoder
 from sklearn.pipeline import make_pipeline
 import pandas as pd
 import numpy as np
-# %%
+
 # if __name__ == '__main__':
 #     client = Client('172.31.2.54:8786')
 # print(client.scheduler_info()['services'])
