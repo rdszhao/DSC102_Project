afdf1b479123cae4addffd7bdffeb84182b00d0b
rdz <rdszhao@gmail.com>
2021-05-28 18:55:35 -0700

    changed model

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,4 +1,4 @@
 source /home/ec2-user/dsc/bin/activate
 sh /home/ec2-user/DSC102_Project/scripts/feature_prep.sh
 sh /home/ec2-user/DSC102_Project/scripts/label_prep.sh
-sh /home/ec2-user/DSC102_Project/scripts/model_prep.sh
+sh /home/ec2-user/DSC102_Project/scripts/model.sh
