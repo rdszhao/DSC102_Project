b9b3dea000aa903ca4c448fb8b91399effc953c9
rdz <rdszhao@gmail.com>
2021-05-28 21:31:01 -0700

    regions

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,7 +1,4 @@
 source /home/ec2-user/dsc/bin/activate
-export AWS_DEFAULT_REGION=us-west-2
 sh /home/ec2-user/DSC102_Project/scripts/feature_prep.sh
-aws s3 cp /home/ec2-user/DSC102_Project/modules/features_parquet s3://ds102-rayz-scratch/features_parquet --recursive
-# rm -r /home/ec2-user/DSC102_Project/modules/features_parquet
 sh /home/ec2-user/DSC102_Project/scripts/label_prep.sh
 sh /home/ec2-user/DSC102_Project/scripts/model.sh
--- scripts/model.sh
+++ scripts/model.sh
@@ -1,1 +1,2 @@
+export AWS_DEFAULT_REGION=us-west-2
 python3 /home/ec2-user/DSC102_Project/modules/model.py
